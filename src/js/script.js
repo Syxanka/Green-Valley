7dbfcc9c866a0548d1e88f3a2b0c1711b30b8006
Kristina Manchenko <cristal89@bk.ru>
2017-02-26 13:05:05 +0300

    first caorusel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+        loop:true, //Зацикливаем слайдер
+        margin: 0, //Отступ от элемента справа в 50px
+        autoplay: true,
+        autoplayTimeout: 1000,
+    });
 });
