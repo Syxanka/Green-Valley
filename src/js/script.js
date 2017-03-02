839555938f8180e55fad88904dbb3d331742ebd6
Kristina Manchenko <cristal89@bk.ru>
2017-03-02 22:18:32 +0300

    мелкие исправленья

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop:true, //Зацикливаем слайдер
         margin: 0, //Отступ от элемента справа в 50px
+        nav: false,
         autoplay: true,
         autoplayTimeout: 1000,
+        navigation:true,
+        pagination:true,
     });
+    
+    $(".room-card__gallery").owlCarousel({
+        items: 1,
+		nav: true,
+		dots: false,
+    })
+    
 });
